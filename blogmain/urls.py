8598ae578df828d62894242aefdc2497e90fded6
Ankit Dware <sabindware@gmail.com>
2024-10-12 17:01:17 +0545

    issues fixed

--- blogmain/urls.py
+++ blogmain/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('deletepost/<int:id>', views.deleteblog, name = "deletepost"),
     path('updatepost/<int:id>', views.updateblog, name = "updatepost"),
     path('userblogs/<str:username>', userblogList.as_view(), name = "userbloglist"),
-    path('create_superuser/', views.create_superuser_view, name = "create_superuser")
+
 ]
